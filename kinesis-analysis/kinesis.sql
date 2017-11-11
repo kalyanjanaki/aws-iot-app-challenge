8aeba761e86a35d156f630e42fc76e7b787b1164
EC2 Default User <ec2-user@ip-172-31-26-73.us-west-2.compute.internal>
2017-11-11 20:39:40 +0000

    Modified kinesis sql

--- kinesis-analysis/kinesis.sql
+++ kinesis-analysis/kinesis.sql
@@ -1,30 +1,51 @@
-CREATE OR REPLACE STREAM "DESTINATION_SQL_STREAM" (
+ CREATE OR REPLACE STREAM "DESTINATION_SQL_STREAM" (
     "fleet" VARCHAR(10), 
+    "load" double,
+    "temperature" double,
+    "terrain" double,
+    "mileage" double,
 	"firstval" double,
 	"lastval" double);
 
+
 CREATE OR REPLACE PUMP "STREAM_PUMP" AS 
     INSERT INTO "DESTINATION_SQL_STREAM"
-    SELECT STREAM "fleet",   
-                  FIRST_VALUE("mileage") OVER W1 AS "firstval",
-                  LAST_VALUE("mileage") OVER W1 AS "lastval"
+    SELECT STREAM "FLEET", 
+                    AVG("LOAD") OVER W1 AS "load",
+                    AVG("TEMPERATURE") OVER W1 AS "temperature",
+                    AVG("TERRAIN") OVER W1 AS "terrain",
+                    MAX("MILEAGE") OVER W1 AS "mileage",
+                  FIRST_VALUE("VELOCITY") OVER W1 AS "firstval",
+                  LAST_VALUE("VELOCITY") OVER W1 AS "lastval"
  FROM "SOURCE_SQL_STREAM_001"
     WINDOW W1 AS (
-        PARTITION BY "fleet" 
+        PARTITION BY "FLEET" 
         RANGE INTERVAL '1' MINUTE PRECEDING);
-        
+
+
  CREATE OR REPLACE STREAM "DESTINATION_SQL_STREAM_DIFF" (
     "fleet" VARCHAR(10), 
-	"distdiff" double);
- CREATE OR REPLACE PUMP "STREAM_PUMP_DIFF" AS 
-    INSERT INTO "DESTINATION_SQL_STREAM_DIFF"
-    SELECT STREAM "fleet", "lastval" - "firstval" AS "distdiff"
-    FROM "DESTINATION_SQL_STREAM";
+    "load" double,
+    "temperature" double,
+    "terrain" double,
+    "mileage" double,
+	"veldiff" double);
 
- CREATE OR REPLACE STREAM "DESTINATION_SQL_STREAM_VEL" (
+
+ CREATE OR REPLACE PUMP "STREAM_PUMP_DIFF" AS 
+	    INSERT INTO "DESTINATION_SQL_STREAM_DIFF"
+	    SELECT STREAM "fleet", "load", "temperature", "terrain", "mileage", "lastval" - "firstval" AS "veldiff"
+	    FROM "DESTINATION_SQL_STREAM";
+	    
+ CREATE OR REPLACE STREAM "DESTINATION_SQL_STREAM_ACC" (
     "fleet" VARCHAR(10), 
-	"velocity" double);
- CREATE OR REPLACE PUMP "STREAM_PUMP_VEL" AS 
-    INSERT INTO "DESTINATION_SQL_STREAM_VEL"
-    SELECT STREAM "fleet", "distdiff"/60 AS "velocity"
-    FROM "DESTINATION_SQL_STREAM_DIFF";
+    "load" double,
+    "temperature" double,
+    "terrain" double,
+    "mileage" double,
+	"acc" double);
+	
+ CREATE OR REPLACE PUMP "STREAM_PUMP_ACC" AS 
+	    INSERT INTO "DESTINATION_SQL_STREAM_ACC"
+	    SELECT STREAM "fleet", "load", "temperature", "terrain", "mileage", "veldiff"/60 AS "acc"
+	    FROM "DESTINATION_SQL_STREAM_DIFF";
