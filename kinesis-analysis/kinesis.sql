718e41c56afdbea95169cc824d29a1144b3ec873
EC2 Default User <ec2-user@ip-172-31-26-73.us-west-2.compute.internal>
2017-11-14 04:50:42 +0000

    Modifed Kinesis Sql for Brakes calculation

--- kinesis-analysis/kinesis.sql
+++ kinesis-analysis/kinesis.sql
@@ -1,3 +1,4 @@
+-- With The Sliding window of the 1 Minute Calculate the Starting Velocity and Ending Velocity.
  CREATE OR REPLACE STREAM "DESTINATION_SQL_STREAM" (
     "fleet" VARCHAR(10), 
     "load" double,
@@ -21,6 +22,9 @@ CREATE OR REPLACE PUMP "STREAM_PUMP" AS
     WINDOW W1 AS (
         PARTITION BY "FLEET" 
         RANGE INTERVAL '1' MINUTE PRECEDING);
+        
+
+-- From Previous In-Memory Stream calculate the Velocity Difference With in the Window Duration.
 
 
  CREATE OR REPLACE STREAM "DESTINATION_SQL_STREAM_DIFF" (
@@ -37,6 +41,8 @@ CREATE OR REPLACE PUMP "STREAM_PUMP" AS
 	    SELECT STREAM "fleet", "load", "temperature", "terrain", "mileage", "lastval" - "firstval" AS "veldiff"
 	    FROM "DESTINATION_SQL_STREAM";
 	    
+-- From the Velocity Difference Calculated in Previous stream calculate the Accleration of deceleration occured in each window.
+	    
  CREATE OR REPLACE STREAM "DESTINATION_SQL_STREAM_ACC" (
     "fleet" VARCHAR(10), 
     "load" double,
@@ -49,3 +55,25 @@ CREATE OR REPLACE PUMP "STREAM_PUMP" AS
 	    INSERT INTO "DESTINATION_SQL_STREAM_ACC"
 	    SELECT STREAM "fleet", "load", "temperature", "terrain", "mileage", "veldiff"/60 AS "acc"
 	    FROM "DESTINATION_SQL_STREAM_DIFF";
+	    
+
+-- From all the previous streams calculate the features such as Average Load on Truck, Average temperature of tires, Average Terrain condition of road and number of brakes applied.
+-- Thos features will help us to identify the tire wear and condition in realtime.
+	    
+ CREATE OR REPLACE STREAM "FINAL_ANALYSIS_STREAM"(
+ "fleet" VARCHAR(10),
+ "load" double,
+ "temperature" double,
+ "terrain" double,
+ "brakes" integer);
+ 
+ CREATE OR REPLACE PUMP "FINAL_ANALYSIS_PUMP" AS
+    INSERT INTO "FINAL_ANALYSIS_STREAM"
+    SELECT STREAM "fleet",                         
+        AVG("load") AS "load",
+        AVG("temperature") AS "temperature",
+        AVG("terrain") AS "terrain",
+        COUNT("acc") as "brakes"
+        FROM "DESTINATION_SQL_STREAM_ACC" where "acc" < 0
+        GROUP BY "fleet",
+        STEP("DESTINATION_SQL_STREAM_ACC".ROWTIME BY INTERVAL '2' MINUTE);
